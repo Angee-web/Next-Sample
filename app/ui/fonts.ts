7d5eed20b3b62ee2dbc1709034c37ad7b5b97750
Adaeze Ugwumba <adaezeugwumba034@gmail.com>
2024-09-22 13:57:03 +0100

    Refactor UI styles and import fonts
    - Refactor UI styles in layout.tsx and page.tsx to improve readability and maintainability.
    - Import global.css and fonts in layout.tsx and page.tsx for consistent styling across the application.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana, Sofadi_One } from "next/font/google";
+
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+    subsets: ["latin"],
+    weight: "400"
+});
+export const sofadi_one = Sofadi_One({
+    subsets: ["latin"],
+    weight: "400"
+});
